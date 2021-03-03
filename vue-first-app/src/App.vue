28288d69891e75e2ae16b25bf4ad24f605987582
anurag1001001 <anuragtripathi23may@gmail.com>
2021-03-03 09:46:34 +0530

    just learnt the concept of component

---
+++ vue-first-app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <header>
+      <h1>
+        My friend List
+      </h1>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+// import FriendContact from "./components/FriendContact.vue";
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "anurag",
+          name: "Anurag",
+          phone: "7899874562",
+          email: "anurag@localhost.com",
+        },
+        {
+          id: "anurag",
+          name: "Anurag",
+          phone: "7899874562",
+          email: "anurag@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
