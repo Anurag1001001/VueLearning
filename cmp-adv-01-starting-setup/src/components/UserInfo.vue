e729361aa266b9cd966f553381c939e723fecaac
anurag1001001 <anuragtripathi23may@gmail.com>
2021-03-04 13:04:18 +0530

    slots styles and compilation

--- cmp-adv-01-starting-setup/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -1,11 +1,14 @@
 <template>
   <section>
     <base-card>
-      <header>
+      <template v-slot:header>
+        <!-- we're passing this markup to baseCard component(child component) but that doesn't mean we're passing props and styles and everything to BaseCard, Vue analyze and evaluate the markup here itself and then it pass to baseCard(here Child) component and this is how Vuejs works(same concept of react) -->
         <h3>{{ fullName }}</h3>
         <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-      </header>
-      <p>{{ infoText }}</p>
+      </template>
+      <template v-slot:default>
+        <p>{{ infoText }}</p>
+      </template>
     </base-card>
   </section>
 </template>
@@ -15,11 +18,3 @@ export default {
   props: ["fullName", "infoText", "role"],
 };
 </script>
-
-<style scoped>
-section header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-</style>
