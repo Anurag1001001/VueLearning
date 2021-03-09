703b08e759f71a94238f549d76757c8a215ef2df
anurag1001001 <anuragtripathi23may@gmail.com>
2021-03-09 19:02:24 +0530

    All about Vuex

---
+++ vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <base-container title="Vuex" v-if="isAuth">
+    <h2>{{ counter }}</h2>
+    <button @click="AddOne">Add 1</button>
+  </base-container>
+
+  <base-container title="Auth">
+    <user-auth></user-auth>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import UserAuth from './components/UserAuth.vue';
+// import { mapGetters } from 'vuex';
+
+export default {
+  components: {
+    BaseContainer,
+    UserAuth
+  },
+  computed: {
+    counter() {
+      return this.$store.getters['number/normalizeCounter'];
+    },
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
+    }
+  },
+  methods: {
+    AddOne() {
+      this.$store.dispatch('number/increment', { value: 10 });
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ vuex-01-starting-setup/src/components/UserAuth.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="logIn" v-if="!userIsAuthenticated">LoggedIn</button>
+  <button @click="logOut" v-else>LoggedOut</button>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters(['userIsAuthenticated'])
+  },
+  methods: {
+    // logIn() {
+    //   this.$store.dispatch('logIn');
+    // },
+    // logOut() {
+    //   this.$store.dispatch('logOut');
+    // },
+    ...mapActions(['logIn', 'logOut'])
+  }
+};
+</script>
